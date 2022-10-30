a3a41e0428a639bd7b06a6858d8d6ce68cf55166
jinhye <jinh2376@gmail.com>
2022-10-30 23:53:17 +0900

    :poop: basic structure
    pooooop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom'
+import './styles/App.css'
+import Today from "./components/Today"
+import Week from "./components/Week";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo'>
+
+      <div> 
+        <div className="title">Today's croffle</div>
+        <div className="Navbar">
+          <li>
+            <NavLink to="/" style={({ isActive }) => ({ color: isActive ? 'pink' : 'white' })}>Today</NavLink>
+          </li>
+          <li>
+            <NavLink to="/week" className={({ isActive }) => "nav-link" + (isActive ? " activated" : "")}>Week</NavLink>
+          </li>
+        </div>
+      </div>
+
+      <Routes>
+        <Route exact path="/" element={<Today />} />
+        <Route path="/week" element={<Week />} />
+      </Routes>
+      
     </div>
+    
   );
-}
+};
 
 export default App;
