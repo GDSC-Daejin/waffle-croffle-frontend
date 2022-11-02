fd8603120aee2b5df9cef9e136067d7792df70d0
jinhye <jinh2376@gmail.com>
2022-11-02 16:25:10 +0900

    :lipstick: modify css
    each class -> one class, each id

--- src/components/Star.js
+++ src/components/Star.js
@@ -3,7 +3,7 @@ import '../styles/pages.css'
 
 const Star = () => {
     return (
-        <div className="star">
+        <div className="page" id="star">
             <h1>Star</h1>
             <p>홈, 그 페이지는 가장 먼저 보여지는 페이지.</p>
         </div>
--- src/components/Today.js
+++ src/components/Today.js
@@ -3,9 +3,10 @@ import '../styles/pages.css'
 
 const Today = () => {
     return (
-        <div className="today">
+        <div className="page" id="today">
             <h1>Today</h1>
-            <p>홈, 그 페이지는 가장 먼저 보여지는 페이지.</p>
+            <p>홈, 그 페이지는 가장 먼저 보여지는 페이지.
+            </p>
         </div>
     )
 }
