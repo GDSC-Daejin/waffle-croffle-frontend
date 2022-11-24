738907ec41852ce6e9adc72a7b6a74fc9548a168
jinhye <jinh2376@gmail.com>
2022-11-24 13:40:22 +0900

    :lipstick: 화면 중앙에 오게 하기
    position absolute 사용함

--- src/AppStyle.js
+++ src/AppStyle.js
@@ -14,7 +14,10 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 export const TodaysCroffle = styled.div`
-  margin: 20% auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
   height: 560px;
   justify-content: center;
