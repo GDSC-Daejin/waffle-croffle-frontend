86b8eab8fc56f0ec766556134d0b4f4e6e797d58
Sujin <tnwlsk0190@gmail.com>
2022-12-25 21:09:23 +0900

    작업중🚧

--- src/components/page/Todo/index.js
+++ src/components/page/Todo/index.js
@@ -12,20 +12,42 @@ import {
   detail,
   star,
   nostar,
+  deleteIcon,
 } from '../../../assets/pageSvg/ExportSvg';
+import axios from 'axios';
 
 const Todo = ({users}) => {
   const [isCheck, setCheck] = useState(false);
   const [isStar, setStar] = useState(false);
+
   const onClickImg = () => {
     setCheck(!isCheck);
+    axios.put('http://localhost:9092/api/todos/' + users.id)
   };
   const onClickStar = () => {
     setStar(!isStar);
   };
 
 
-  const [isTodo = '22', setTodo] = useState();
+  const checkStar = () => {
+    // setView(isTodo({ ...isTodo }));
+    // console.log(isView);
+    axios
+      .post('http://localhost:9092/api/todos')
+      .then(function (response) {
+        console.log('ok');
+      })
+      .catch(function (error) {
+        console.log('error');
+      });
+  };
+
+  const onDelete = () => {
+    axios.delete('http://localhost:9092/api/todos/' + users.id)
+  };
+
+  console.log(users.id)
+
   return (
     <TodoWrapper>
       <TodoImg
@@ -37,12 +59,12 @@ const Todo = ({users}) => {
         color={isCheck ? '#999999' : '#f1f1f1'}
         deco={isCheck ? 'line-through' : 'none'}
       >
-        <TodoContent>{isTodo}</TodoContent>
+        <TodoContent>{users.content}</TodoContent>
         <TodoCategory>
           카테고리
         </TodoCategory>
       </TodoText>
-      <TodoImg src={detail} />
+      <TodoImg src={deleteIcon} onClick={onDelete}/>
       <TodoImg src={isStar ? star : nostar} onClick={onClickStar} />
     </TodoWrapper>
   );
